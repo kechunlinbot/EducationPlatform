bb277079a825b0ae93b852059f19ef2444e94d4d
dengguoguo <33865711+dengguoguo@users.noreply.github.com>
2017-12-29 21:49:07 +0800

    Update __init__.py

--- EducationPlatform/__init__.py
+++ EducationPlatform/__init__.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#author dengguo
-=======
->>>>>>> 0b9227b4096eb0cb578badbe9e9ee05489cb6403
-=======
->>>>>>> 0b9227b4096eb0cb578badbe9e9ee05489cb6403
 import pymysql
 
 pymysql.install_as_MySQLdb()
