815babf61d300ca3b3a82f08c27871d82ffe1be8
sharonchiong <sharonchiong@foxmail.com>
2018-01-03 16:23:05 +0800

    Auto stash before merge of "develop" and "origin/develop"
    add homework models, update knowledgequestions models.

--- apps/homework/models.py
+++ apps/homework/models.py
@@ -2,17 +2,46 @@ from django.db import models
 from datetime import datetime
 
 from classes.models import Class
+from teachers.models import Teacher
+from knowledgequestions.models import KnowledgeBase, QuestionBank
+
+IS_BONUS = (
+    ('1', '是'),
+    ('0', '否'),
+)
+
+IS_COMPLETE = IS_BONUS
 
 
-# class HomeWork(models.Model):
-#     name = models.CharField(max_length=30, unique=True, verbose_name='题目名')
-#     content = models.TextField(verbose_name='内容')
-#     class_work = models.ForeignKey(Class, verbose_name='班级')
-#     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
-#
-#     class Meta:
-#         verbose_name = '作业管理'
-#         verbose_name_plural = verbose_name
-#
-#     def __repr__(self):
-#         return self.name
+class Assignment(models.Model):
+    question = models.CharField(QuestionBank, verbose_name='题目')
+    class_name = models.ForeignKey(Class, verbose_name='班级')
+    teacher = models.ForeignKey(Teacher, verbose_name='教师')
+    module = models.ForeignKey(KnowledgeBase, verbose_name='知识模块')
+    is_bonus = models.CharField(max_length=1, choices=IS_BONUS, default='0', verbose_name='加分题')
+    refer_complete_time = models.CharField(max_length=5, default='10', verbose_name='参考完成时间(分钟数)')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '作业布置'
+        verbose_name_plural = verbose_name
+        db_table = 'assignments'
+
+    def __repr__(self):
+        return self.question
+
+
+class Submission(models.Model):
+    assignment = models.ForeignKey(Assignment, verbose_name='作业')
+    is_complete = models.CharField(max_length=1, choices=IS_COMPLETE, default=0, verbose_name='题目状态')
+    complete_time = models.IntegerField(max_length=5, default=0, verbose_name='完成时间(分钟数)')
+    py_file_path = models.CharField(max_length=200, default=0, verbose_name='.py文件路径')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '作业提交'
+        verbose_name_plural = verbose_name
+        db_table = 'submissions'
+
+    def __repr__(self):
+        return '%s:%s' % (self.assignment, self.is_complete)
--- apps/knowledgequestions/models.py
+++ apps/knowledgequestions/models.py
@@ -2,11 +2,42 @@
 from django.db import models
 from datetime import datetime
 
+from teachers.models import Teacher
+from classes.models import Class
+
+DIFFICULTY_LEVELS = (
+    ('beginner', '初级'),
+    ('intermediate', '中级'),
+    ('advanced', '高级'),
+)
+
+GRADES = (
+    ('first', '一级模块'),
+    ('second', '二级模块'),
+)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30, verbose_name='课程名字')
+    detail = models.TextField(verbose_name='课程详情')
+    difficulty_level = models.CharField(max_length=6, choices=DIFFICULTY_LEVELS, default='beginner', verbose_name='难易程度')
+    category = models.CharField(max_length=20, default='后端', verbose_name='课程类型')
+    teacher = models.ForeignKey(Teacher, verbose_name='教师', null=True, blank=True)
+    class_name = models.ForeignKey(Class, verbose_name='班级', null=True, blank=True)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程'
+        verbose_name_plural = verbose_name
+        db_table = 'courses'
+
+    def __repr__(self):
+        return self.name
+
 
 class KnowledgeBase(models.Model):
-    course_name = models.CharField(max_length=20, verbose_name='课程名称')
-    first_module = models.CharField(max_length=30, verbose_name='一级模块')
-    second_module = models.CharField(max_length=30, verbose_name='二级模块')
+    course = models.ForeignKey(Course, verbose_name='课程')
+    module = models.CharField(max_length=6, choices=GRADES, default='first', verbose_name='知识模块')
     knowledge_point = models.CharField(max_length=100, null=True, blank=True, verbose_name='知识点')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
@@ -16,20 +47,13 @@ class KnowledgeBase(models.Model):
         db_table = 'knowledgebase'
 
     def __repr__(self):
-        return self.course_name
-
-
-DIFFICULTY_LEVELS = (
-    ('beginner', '初级'),
-    ('intermediate', '中级'),
-    ('advanced', '高级'),
-)
+        return '%s:%s' % (self.course, self.module)
 
 
 class QuestionBank(models.Model):
     title = models.CharField(max_length=50, verbose_name='题目名称')
     content = models.TextField(verbose_name='题目内容')
-    difficulty_level = models.CharField(max_length=6, choices=DIFFICULTY_LEVELS, verbose_name='难易程度')
+    difficulty_level = models.CharField(max_length=6, choices=DIFFICULTY_LEVELS, default='beginner', verbose_name='难易程度')
     knowledge_module = models.ForeignKey(KnowledgeBase, verbose_name='知识模块')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
